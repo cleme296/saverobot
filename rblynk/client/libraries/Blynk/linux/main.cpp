ed252233f30c9e62aba6233d2974ed99ff4e9bbd
cleme296 <cgaillar@les-2-rives.fr>
2018-03-27 12:41:15 +0000

    first commit

---
+++ rblynk/client/libraries/Blynk/linux/main.cpp
@@ -0,0 +1,86 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+
+  Fade using a LED widget on your phone!
+
+  App project setup:
+    LED widget on V2
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT stdout
+
+#ifdef RASPBERRY
+ #include <BlynkApiWiringPi.h>
+#else
+ #include <BlynkApiLinux.h>
+#endif
+#include <BlynkSocket.h>
+#include <BlynkOptionsParser.h>
+
+static BlynkTransportSocket _blynkTransport;
+BlynkSocket Blynk(_blynkTransport);
+#include <BlynkWidgets.h>
+
+WidgetLED led2(V2);
+
+BlynkTimer timer;
+
+// V2 LED Widget is fading
+void fadeLedWidget()
+{
+  static int value = 0;
+  static int delta = 30;
+  value += delta;
+  if (value > 255 || value < 0) {
+    delta = -delta;
+  } else {
+    std::cout << "LED on V2: ";
+    std::cout << value;
+    led2.setValue(value);
+  }
+}
+
+void setup()
+{
+  timer.setInterval(300L, fadeLedWidget);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
+
+int main(int argc, char* argv[])
+{
+  const char *auth, *serv;
+  uint16_t port;
+  parse_options(argc, argv, auth, serv, port);
+
+  Blynk.begin(auth, serv, port);
+
+  setup();
+  while(true) {
+    loop();
+  }
+
+  return 0;
+}
